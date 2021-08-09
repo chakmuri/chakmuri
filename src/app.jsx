d92b950ea34854c6531b93d528b13ddeeb21fe32
Dev-seongjaeKIM <ayj0111yaj5@gmail.com>
2021-08-09 18:29:21 +0900

    Project initial setting

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default App;
