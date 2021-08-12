d0a730645ed8b56180936846baceae0e0e400f92
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2021-08-12 21:34:52 +0900

    Project initial setting

---
+++ back/chakmuri/src/test/java/bookclub/chakmuri/ChakmuriApplicationTests.java
@@ -0,0 +1,13 @@
+package bookclub.chakmuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChakmuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
