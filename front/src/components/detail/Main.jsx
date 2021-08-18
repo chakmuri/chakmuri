3e8e915442a52f1496c59849bf70fc5f4f11a666
Dev-seongjaeKIM <ayj0111yaj5@gmail.com>
2021-08-18 21:53:12 +0900

    chore: Added comment layout

--- front/src/components/detail/Main.jsx
+++ front/src/components/detail/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components";
 import Summary from '../detail/Summary';
 import Introduction from './Introduction';
+import Comments from './Comments';
 
 /**
  * 크게 
  * 
  *  1. 요약<Summary>와
  *  2. 상세 설명<Introduction> == 우리 독서 모임은 ...
+ *  3. 이 독서모임 댓글(가제)
  * 
  * 으로 나누었습니다.
  */
@@ -17,6 +19,7 @@ const Wrapper = styled.div`
 	margin: 0 auto;
 
   text-align: center; /* 가운데 정렬 */
+  margin-bottom: 50px;
 `;
 
 const Main = (props) => {
@@ -25,6 +28,7 @@ const Main = (props) => {
       <Wrapper>
         <Summary />
         <Introduction />
+        <Comments />
       </Wrapper>
     </>
   );
