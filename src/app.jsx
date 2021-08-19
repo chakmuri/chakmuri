17ba5bda57f48be9007c925a50fa9b6550c4f5ad
Dev-seongjaeKIM <ayj0111yaj5@gmail.com>
2021-08-19 18:47:54 +0900

    docs: Updated gitignore && .jsx files

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Board from "../src/pages/board";
+import Board from "../src/pages/Board";
 import { Switch, Route } from "react-router-dom";
 
 const App = () => {
--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from '../components/common/navbar'; // 임시
-import Main from '../components/board/main';
-import Footer from '../components/common/footer'; // 임시
+import Navbar from '../components/common/Navbar'; // 임시
+import Main from '../components/board/Main';
+import Footer from '../components/common/Footer'; // 임시
 
 import DeadlineTag from '../components/common/deadlineTag'; // 일단 Footer 밑에 두겠습니다...
 
