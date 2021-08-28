da2b7927eb9d2af3b6a93947528fc604787d3767
Sol-Ahn <sorry0194@gmail.com>
2021-08-28 22:46:06 +0900

    feat: ScrollToTop 컴포넌트 추가

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -5,15 +5,18 @@ import Board from "../src/pages/Board";
 import Detail from "../src/pages/Detail";
 import MyPage from "./pages/MyPage";
 import "antd/dist/antd.css";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 const App = () => {
 	return (
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/board" component={Board} />
-			<Route exact path="/detail:id" component={Detail} />
-			<Route exact path="/myPage" component={MyPage} />
-		</Switch>
+		<ScrollToTop>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/board" component={Board} />
+				<Route exact path="/detail:id" component={Detail} />
+				<Route exact path="/myPage" component={MyPage} />
+			</Switch>
+		</ScrollToTop>
 	);
 };
 
