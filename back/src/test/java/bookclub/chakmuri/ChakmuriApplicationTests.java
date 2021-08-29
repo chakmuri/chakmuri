f4384fbec0d6ef216ef2c2b012d9c88e5c777ed6
Sol-Ahn <sorry0194@gmail.com>
2021-08-29 18:34:12 +0900

    fix: 스프링 부트 프로젝트 빌드 에러 해결

--- back/src/test/java/bookclub/chakmuri/ChakmuriApplicationTests.java
+++ back/src/test/java/bookclub/chakmuri/ChakmuriApplicationTests.java
@@ -3,7 +3,7 @@ package bookclub.chakmuri;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ChakmuriApplicationTests {
 
 	@Test
