b03911b97915f533472d7e776fcfb0934409c461
sharpie1330 <amberaplin14@gmail.com>
2021-09-13 20:39:33 +0900

    build: FE build 파일 추가 및 테스트 5

--- back/src/main/java/bookclub/chakmuri/controller/club/ClubController.java
+++ back/src/main/java/bookclub/chakmuri/controller/club/ClubController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- back/src/main/java/bookclub/chakmuri/service/ClubService.java
+++ back/src/main/java/bookclub/chakmuri/service/ClubService.java
@@ -7,7 +7,6 @@ import bookclub.chakmuri.controller.club.ClubUpdateRequestDto;
 import bookclub.chakmuri.domain.*;
 import bookclub.chakmuri.repository.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
